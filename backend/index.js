ecf25e2a21927ba102d0c080a9fbb54f421efc67
Bless Ziamah <tziamah1@gmail.com>
2024-03-04 03:08:22 +0000

    fix to backend-index.jsx

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,19 @@ import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 import {Book} from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 
-// app.use(cors())
+app.use(cors())
 
-app.use(cors({
-	origin: "http://localhost:5000",
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-	allowedHeaders: ['Content-Type']
-}))
+// app.use(cors({
+// 	origin: "http://localhost:5000",
+// 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// 	allowedHeaders: ['Content-Type']
+// }))
 
 app.get('/', (req, res) => {
 	console.log(req)
