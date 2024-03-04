00363f65fcbb7dc160222728db33c93f8266ef87
Bless Ziamah <tziamah1@gmail.com>
2024-03-04 03:02:47 +0000

    Homepage update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import DeleteBook from "./pages/DeleteBook.jsx";
+import ShowBook from "./pages/ShowBook.jsx";
+import EditBook from "./pages/EditBook.jsx";
+import CreateBooks from "./pages/CreateBooks.jsx";
 
 const App = () => {
   return(
     <Routes>
-      <Route path={} element={} />
-      <Route path={} element={} />
-      <Route path={} element={} />
-      <Route path={} element={} />
-      <Route path={} element={} />
+      <Route path={"/"} element={<Home />} />
+      <Route path={"/books/create"} element={<CreateBooks />} />
+      <Route path={"/books/details/:id"} element={<ShowBook />} />
+      <Route path={"/books/edit/:id"} element={<EditBook />} />
+      <Route path={"/books/delete/:id"} element={<DeleteBook />} />
     </Routes>
   )
 }
