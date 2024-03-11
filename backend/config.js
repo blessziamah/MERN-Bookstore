964a8b79bfb19cb0e979a3a824b54d1743ca1c06
Bless Ziamah <tziamah1@gmail.com>
2024-03-11 13:53:07 +0000

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
  export const PORT = 5000
 
- export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.nebs8kw.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-Mern'
+ export const mongoDBURL = "your mongodburl"
